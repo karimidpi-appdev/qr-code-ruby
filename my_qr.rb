13fa26a2377590456aef58a20098dd69e82037a9
Mohammad Ali Karimi <ali216k@gmail.com>
2023-02-20 20:05:39 +0000

    Encoding URLs

--- my_qr.rb
+++ my_qr.rb
@@ -2,7 +2,7 @@ p "hi"
 require "rqrcode"
 
 # Use the RQRCode::QRCode class to encode some text
-qrcode = RQRCode::QRCode.new("wikipedia qr code")
+qrcode = RQRCode::QRCode.new("https://en.wikipedia.org/wiki/QR_code")
 
 # Use the .as_png method to create a 500 pixels by 500 pixels image
 png = qrcode.as_png({ :size => 500 })
