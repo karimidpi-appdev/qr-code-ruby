37d3f3c683123b499bd823e9a5047579c77d1cdb
Mohammad Ali Karimi <ali216k@gmail.com>
2023-02-20 19:56:50 +0000

    installing and using the rqrcode gem

---
+++ my_qr.rb
@@ -0,0 +1,11 @@
+p "hi"
+require "rqrcode"
+
+# Use the RQRCode::QRCode class to encode some text
+qrcode = RQRCode::QRCode.new("wikipedia qr code")
+
+# Use the .as_png method to create a 500 pixels by 500 pixels image
+png = qrcode.as_png({ :size => 500 })
+
+# Write the image data to a file
+IO.binwrite("sometext.png", png.to_s)
